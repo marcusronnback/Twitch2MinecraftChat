9d6ae6dbff56d23d13587475dda4c2e8bc092a35
Marcus Rönnbäck <marcus@ronnback.tech>
2021-05-12 02:51:47 +0200

    Cleanup of legacy code

---
+++ src/main/java/com/github/marcusronnback/twitch2minecraftchat/commands/CommandT2mcBotStop.java
@@ -0,0 +1,21 @@
+package com.github.marcusronnback.twitch2minecraftchat;
+
+import com.github.marcusronnback.twitch2minecraftchat.BotManager;
+import com.github.marcusronnback.twitch2minecraftchat.ChatUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandT2mcBotStop implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length > 0) return  false;
+        if(BotManager.getInstance().stop()){
+            ChatUtils.log(sender,"Stopped successfully");
+        }else{
+            ChatUtils.warn(sender,"Already stopped!");
+        }
+        return true;
+    }
+}
